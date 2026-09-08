5615c6bfb77b002c9c7ac1cc71b3b100830f1eb5
Makar557 <makatdy@gmail.com>
2026-09-08 13:24:11 +0300

    user-service(fix)

--- src/main/java/dybr/dev/task3_spring/model/UserOperation.java
+++ src/main/java/dybr/dev/task3_spring/model/UserOperation.java
@@ -1,16 +1,14 @@
 package dybr.dev.task3_spring.model;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Getter
 public enum UserOperation {
 
     USER_DELETION("Здравствуйте! Ваш аккаунт был удалён."),
     USER_CREATION("Здравствуйте! Ваш аккаунт на сайте был успешно создан.");
 
     private final String message;
-
-    public String getMessage() {
-        return message;
-    }
 }
